93f8e6fb396528da813e70a8ae1cfb43afe7b665
Bryan Gale <bryan@bryangale.com>
2024-01-24 12:09:21 +0000

    Use warp to serve HTTP requests
    This necessitates a move to the tokio runtime.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use clap::Parser;
-use serialport::{SerialPort, SerialPortType};
+use serialport::{SerialPort, SerialPortInfo, SerialPortType};
 use std::{
     io::{prelude::*, BufRead, BufReader, BufWriter},
-    net::TcpListener,
+    sync::{Arc, Mutex},
     thread::sleep,
     time::Duration,
+};
+use warp::{
+    http::StatusCode,
+    reject::Rejection,
+    reply::{Reply, WithStatus},
+    Filter,
 };
 
 #[derive(Parser)]
@@ -15,7 +21,8 @@ struct Cli {
     port: Option<u16>,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let cli = Cli::parse();
     let port_number = cli.port.unwrap_or(7878);
 
@@ -26,55 +33,36 @@ fn main() {
         serial_port.name().unwrap_or("unknown".to_string())
     );
 
-    let tcp_listener = TcpListener::bind(format!("0.0.0.0:{}", port_number)).unwrap();
+    let plotter_handler = create_plotter_handler(&*serial_port);
 
-    println!("Waiting for TCP connection...");
+    let (_, server) = warp::serve(plotter_handler).bind_with_graceful_shutdown(
+        ([0, 0, 0, 0], port_number),
+        async move {
+            tokio::signal::ctrl_c().await.unwrap();
+        },
+    );
 
-    for tcp_stream in tcp_listener.incoming() {
-        println!("TCP connection opened");
-
-        let tcp_stream = tcp_stream.unwrap();
-
-        let tcp_reader_lines = BufReader::new(tcp_stream.try_clone().unwrap()).lines();
-        let mut serial_reader_lines = BufReader::new(serial_port.try_clone().unwrap()).lines();
-
-        let mut serial_writer = BufWriter::new(serial_port.try_clone().unwrap());
-        let mut tcp_writer = BufWriter::new(tcp_stream.try_clone().unwrap());
-
-        for line in tcp_reader_lines.take_while(|line| line.is_ok()) {
-            let mut line = line.unwrap();
-            println!("Request: {}", &line);
-
-            line.push('\r');
-            serial_writer.write_all(line.as_str().as_bytes()).unwrap();
-            serial_writer.flush().unwrap();
-
-            let response = serial_reader_lines.next().unwrap().unwrap();
-            tcp_writer.write_all(response.as_str().as_bytes()).unwrap();
-
-            println!("Repsonse: {}", &response);
-        }
-
-        println!("TCP connection closed. Waiting for new connection...");
-    }
+    let _ = tokio::task::spawn(server).await;
 }
 
 fn get_serial_port() -> Box<dyn SerialPort> {
+    let port_filter = |port_info: &&SerialPortInfo| {
+        if let SerialPortType::UsbPort(usb_port_info) = &port_info.port_type {
+            usb_port_info
+                .product
+                .as_ref()
+                .unwrap_or(&"".to_string())
+                .contains("EiBotBoard")
+        } else {
+            false
+        }
+    };
+
     let port_info = loop {
         let port_info = serialport::available_ports()
             .unwrap_or_default()
             .iter()
-            .find(|port_info| {
-                if let SerialPortType::UsbPort(usb_port_info) = &port_info.port_type {
-                    usb_port_info
-                        .product
-                        .as_ref()
-                        .unwrap_or(&"".to_string())
-                        .contains("EiBotBoard")
-                } else {
-                    false
-                }
-            })
+            .find(port_filter)
             .cloned();
 
         if let Some(port_info) = port_info {
@@ -88,4 +76,48 @@ fn get_serial_port() -> Box<dyn SerialPort> {
         .timeout(Duration::from_secs(1))
         .open()
         .unwrap_or_else(|_| panic!("Could not create port on {}", &port_info.port_name))
+}
+
+fn create_plotter_handler(
+    serial_port: &dyn SerialPort,
+) -> impl warp::Filter<Extract = (WithStatus<impl Reply>,), Error = Rejection> + Clone {
+    let serial_port = Arc::new(Mutex::new(serial_port.try_clone().unwrap()));
+
+    let handler = move |command_bytes: warp::hyper::body::Bytes| {
+        if let Ok(command) = String::from_utf8(command_bytes.to_vec()) {
+            if command.contains('\r') || command.contains('\n') {
+                warp::reply::with_status(warp::reply(), warp::http::StatusCode::BAD_REQUEST)
+            } else {
+                send_to_serial_and_wait_for_ok(&**serial_port.lock().unwrap(), command.as_str());
+                warp::reply::with_status(warp::reply(), StatusCode::OK)
+            }
+        } else {
+            warp::reply::with_status(warp::reply(), StatusCode::BAD_REQUEST)
+        }
+    };
+
+    warp::post()
+        .and(warp::path::end())
+        .and(warp::filters::body::bytes())
+        .map(handler)
+}
+
+fn send_to_serial_and_wait_for_ok(serial_port: &dyn SerialPort, command: &str) {
+    println!("Writing to serial port: {}", command);
+
+    let mut serial_reader_lines = BufReader::new(serial_port.try_clone().unwrap()).lines();
+
+    let mut serial_writer = BufWriter::new(serial_port.try_clone().unwrap());
+    serial_writer
+        .write_all(format!("{}\r", command).as_bytes())
+        .unwrap();
+    serial_writer.flush().unwrap();
+
+    let response = loop {
+        if let Ok(response) = serial_reader_lines.next().unwrap() {
+            break response;
+        }
+    };
+
+    println!("Repsonse from serial port: {}", &response);
 }
