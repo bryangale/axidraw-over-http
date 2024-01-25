fcbf2beaa76c7a557f7f1b1f79a4a5b997c4436f
Bryan Gale <bryan@bryangale.com>
2024-01-25 16:03:07 +0000

    Move to accepting command batches
    We now expose a batch-queue route.

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # axidraw-over-http
 
-A bad minimal utility to expose an AxiDraw plotter over HTTP. Exposes an HTTP server (by default on
-port 7878), and forwards the body of any POST requests to an AxiDraw on a serial port.
+A bad minimal utility to expose an AxiDraw plotter over HTTP.
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use clap::Parser;
 use serialport::{SerialPort, SerialPortInfo, SerialPortType};
 use std::{
+    collections::VecDeque,
     convert::Infallible,
     io::{prelude::*, BufRead, BufReader, BufWriter},
-    sync::{Arc, Mutex},
-    thread::sleep,
+    str::FromStr,
+    sync::Arc,
+    thread::{sleep, spawn},
     time::Duration,
 };
+use tokio::sync::Mutex;
 use warp::{
     reject::Rejection,
     reply::{Reply, WithStatus},
@@ -33,7 +36,21 @@ async fn main() {
         serial_port.name().unwrap_or("unknown".to_string())
     );
 
-    let plotter_handler = create_plotter_handler(&*serial_port);
+    let command_buffer = Arc::new(Mutex::new(VecDeque::<String>::new()));
+    let plotter_handler_command_buffer = command_buffer.clone();
+
+    spawn(move || loop {
+        let mut command_buffer = command_buffer.blocking_lock();
+
+        let command = command_buffer.pop_front();
+        drop(command_buffer);
+
+        if let Some(command) = command {
+            send_to_serial_and_wait_for_ok(&*serial_port, command.as_str());
+        }
+    });
+
+    let plotter_handler = create_plotter_handler(plotter_handler_command_buffer);
 
     let (_, server) = warp::serve(plotter_handler).bind_with_graceful_shutdown(
         ([0, 0, 0, 0], port_number),
@@ -79,26 +96,26 @@ fn get_serial_port() -> Box<dyn SerialPort> {
 }
 
 fn create_plotter_handler(
-    serial_port: &dyn SerialPort,
+    command_buffer: Arc<Mutex<VecDeque<String>>>,
 ) -> impl warp::Filter<Extract = (WithStatus<impl Reply>,), Error = Rejection> + Clone {
-    let serial_port = Arc::new(Mutex::new(serial_port.try_clone().unwrap()));
-
     async fn handler(
         command_bytes: warp::hyper::body::Bytes,
-        serial_port: Arc<Mutex<Box<dyn SerialPort>>>,
+        command_buffer: Arc<Mutex<VecDeque<String>>>,
     ) -> Result<WithStatus<impl Reply>, Infallible> {
-        if let Ok(command) = String::from_utf8(command_bytes.to_vec()) {
-            if command.contains('\r') || command.contains('\n') {
+        if let Ok(body_bytes) = String::from_utf8(command_bytes.to_vec()) {
+            if body_bytes.contains('\r') {
                 Ok(warp::reply::with_status(
                     warp::reply(),
                     warp::http::StatusCode::BAD_REQUEST,
                 ))
             } else {
-                tokio::task::spawn_blocking(move || {
-                    send_to_serial_and_wait_for_ok(&**serial_port.lock().unwrap(), command.as_str())
-                })
-                .await
-                .unwrap();
+                let mut command_buffer = command_buffer.lock().await;
+                for command in body_bytes.split('\n') {
+                    if !command.is_empty() {
+                        command_buffer.push_back(String::from_str(command).unwrap());
+                    }
+                }
+
                 Ok(warp::reply::with_status(
                     warp::reply(),
                     warp::http::StatusCode::OK,
@@ -113,9 +130,9 @@ fn create_plotter_handler(
     }
 
     warp::post()
-        .and(warp::path::end())
+        .and(warp::path("batch-queue"))
         .and(warp::filters::body::bytes())
-        .and(warp::any().map(move || serial_port.clone()))
+        .and(warp::any().map(move || command_buffer.clone()))
         .and_then(handler)
 }
 
